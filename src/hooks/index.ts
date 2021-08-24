effa69f02a12a4aa9bec44cb05b379d12dc60725
Myoung Jin Jang <thinkjin6@gmail.com>
2021-08-24 14:51:07 +0900

    [#3] feat: 구현 custom hooks
    Resolves #3

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+import useInput from './useInput';
+import useLocalStorage from './useLocalStorage';
+
+export { useInput, useLocalStorage };
---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+type UseLocalStorage<T> = [T, React.Dispatch<T>];
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const useLocalStorage = <_, T>(
+  key: string,
+  initialValue: T
+): UseLocalStorage<T> => {
+  const [value, setValue] = useState<T>(() => {
+    const savedValue: string | null = localStorage.getItem(key);
+
+    return savedValue ? JSON.parse(savedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;

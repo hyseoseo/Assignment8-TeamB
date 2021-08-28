98e5ebbc8eee809ab69d977b752072075236fed6
Myoung Jin Jang <thinkjin6@gmail.com>
2021-08-28 15:49:30 +0900

    [#44] refactor: 파일 구조 및 위치  리팩토링

--- src/components/todo/TodoController.tsx
+++ src/components/todo/TodoController.tsx
@@ -1,40 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { css } from '@emotion/react';
-import { COLOR_STYLE, MainBtn, MainSelectedBtn } from 'styles';
-import { OPTIONS, Itodo, Status } from './type';
+import { COLOR_STYLE, MainBtn } from 'styles';
+import { TodoFilter } from '.';
+import { FilterType, Status } from './type';
 
 interface Iprop {
-  todo: Itodo;
-  changeTodoStatus: (id: number, status: Status) => void;
-  changeTodoImportance: (id: number) => void;
+  sortTodo: () => void;
+  filterList: (filterType: FilterType, status?: Status) => void;
 }
 
-const TodoController: React.FC<Iprop> = ({ todo, changeTodoStatus, changeTodoImportance }) => {
-  const [status, setStatus] = useState<Status>(todo.status);
-
-  const handleClick = (status: Status): void => {
-    setStatus(status);
-    changeTodoStatus(todo.id, status);
-  };
-
+const TodoController: React.FC<Iprop> = ({ filterList, sortTodo }) => {
   return (
     <div css={Container}>
-      <button
-        css={todo.isImportant ? MainSelectedBtn : MainBtn}
-        onClick={() => changeTodoImportance(todo.id)}
-      >
-        <span css={status === Status.done && Done}>Bookmark</span>
+      <TodoFilter filterList={filterList} />
+      <button css={SortBtn} onClick={sortTodo}>
+        <span>Sort by newest</span>
       </button>
-      {OPTIONS.map((option, index) => (
-        <button
-          type="button"
-          key={index}
-          css={option === status ? MainSelectedBtn : MainBtn}
-          onClick={() => handleClick(option)}
-        >
-          <span css={status === Status.done && Done}>{option}</span>
-        </button>
-      ))}
     </div>
   );
 };
@@ -43,12 +24,20 @@ export default TodoController;
 
 const Container = css`
   display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  margin-top: 0.5rem;
+  justify-content: space-between;
+  align-items: flex-end;
 `;
 
-const Done = css`
-  color: ${COLOR_STYLE.greyDarker};
-  text-decoration: line-through;
+const SortBtn = css`
+  ${MainBtn}
+
+  &:hover {
+    transform: translateY(0);
+    background-color: ${COLOR_STYLE.primary};
+    color: ${COLOR_STYLE.white};
+  }
+
+  &:active {
+    transform: translateY(-3px);
+  }
 `;
---
+++ src/components/todo/TodoStatus.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { css } from '@emotion/react';
+import { COLOR_STYLE, FONT_SIZE_STYLE, MainBtn, MainSelectedBtn } from 'styles';
+import { OPTIONS, Itodo, Status } from './type';
+
+interface Iprop {
+  todo: Itodo;
+  time: string;
+  changeTodoStatus: (id: number, status: Status) => void;
+  toggleBookmark: (id: number) => void;
+}
+
+const TodoStatus: React.FC<Iprop> = ({ todo, time, changeTodoStatus, toggleBookmark }) => {
+  const [status, setStatus] = useState<Status>(todo.status);
+
+  const handleClickStatus = (status: Status): void => {
+    setStatus(status);
+    changeTodoStatus(todo.id, status);
+  };
+
+  return (
+    <div css={Container}>
+      <div css={ControlBox}>
+        <button
+          css={todo.isImportant ? MainSelectedBtn : MainBtn}
+          onClick={() => toggleBookmark(todo.id)}
+        >
+          <span css={status === Status.done && Done}>Bookmark</span>
+        </button>
+        {OPTIONS.map((option, index) => (
+          <button
+            type="button"
+            key={index}
+            css={option === status ? MainSelectedBtn : MainBtn}
+            onClick={() => handleClickStatus(option)}
+          >
+            <span css={status === Status.done && Done}>{option}</span>
+          </button>
+        ))}
+      </div>
+      <small css={LastUpdate}>Updated {time} ago</small>
+    </div>
+  );
+};
+
+export default TodoStatus;
+
+const Container = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+const ControlBox = css`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 0.5rem;
+`;
+
+const Done = css`
+  color: ${COLOR_STYLE.greyDarker};
+  text-decoration: line-through;
+`;
+
+const LastUpdate = css`
+  color: ${COLOR_STYLE.greyDarkest};
+  font-size: ${FONT_SIZE_STYLE.smaller};
+  transform: translateY(-3px);
+`;
